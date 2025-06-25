61e93bd9b06bd192fe6c68c537f52b43addac6dc
Bill <bill.omondi@student.moringaschool.com>
2025-06-25 07:43:32 +0300

    User model added

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
