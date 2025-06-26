d05e2422fcd68afd6aa6455a00f00c8fd773c670
Bill <bill.omondi@student.moringaschool.com>
2025-06-26 15:49:54 +0300

    Crud testing

--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
-from datetime import datetime
- 
+from datetime import datetime, timezone
 
- 
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -12,7 +10,9 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    favorites = db.relationship('Favorite', back_populates='user', cascade='all, delete-orphan')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
