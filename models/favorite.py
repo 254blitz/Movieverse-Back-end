b4bcca5176b9a908e098a5e3c2c7604dfa73e1ee
Bill <bill.omondi@student.moringaschool.com>
2025-06-30 13:23:50 +0300

    Favorites imported

--- models/favorite.py
+++ models/favorite.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import db
 from . import db 
+from models.favorite import Favorite
 
 favorites_bp = Blueprint('favorites', __name__)
 
